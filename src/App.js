a7daeb3666ce8ea2f7c955528301e8e376be4e81
yansika21 <yansika.learner21@gmail.com>
2022-08-17 14:15:13 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Contacts from './components/contacts/Contacts';
+import EditContact from './components/contacts/EditContact';
+import Header from './components/layout/Header';
+import NotFound from './components/pages/NotFound';
+
+import { Provider } from './context';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Header = props => {
+  const { branding } = props;
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-3 py-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          {branding}
+        </a>
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                <i className="fas fa-home" /> Home
+              </Link>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+Header.defaultProps = {
+  branding: 'My App'
+};
+
+Header.propTypes = {
+  branding: PropTypes.string.isRequired
+};
+
+export default Header;
